bfcad1456cf0857f72d4c6307ad3500b8c2d7575
Bhashkar Gupta <bhashkar.spi@gmail.com>
2023-06-23 16:12:54 +0530

    Learning

--- Max_Min.c
+++ Max_Min.c
@@ -1,20 +1,31 @@
 // Write a program to find Max and Min from a string. 
 #include<stdio.h>
 
-int main(){
-    int string[] = {2, 25, 78, 66, 97 ,34 ,5, 12 ,35, 27, 107, 687};
-    int max, min = string[0];
-    int* ptr = string;
-    int strlen = sizeof(string)/sizeof(string[0]);
+void minMax(int *arr, int *min, int *max);
 
-    for(int i = 1; i < strlen ; i++){
-        if(max < *(ptr+i)){
-            max = *(ptr+i);
+int main(){
+    int string[] = {2, 25, 78, 66, 97, 34, 5, 12, 35, 27, 107, 687};
+    int max, min;
+    minMax(string, &min, &max);
+    printf("Max: %d, Min: %d\n", max, min);
+    return 0;
+}
+
+void minMax(int *arr, int *min, int *max){
+    *max = *min = arr[0];
+    int *ptr = arr;
+    int size = 0;
+
+    while (*(ptr + size) != '\0') {
+        size++;
+    }
+
+    for(int i = 1; i < size; i++){
+        if(*max < *(ptr+i)){
+            *max = *(ptr+i);
         }
-        if(min > *(ptr+i)){
-            min = *(ptr+i);
+        if(*min > *(ptr+i)){
+            *min = *(ptr+i);
         }
     }
-    printf("Max: %d, Min: %d\n", max, min);
-
 }
