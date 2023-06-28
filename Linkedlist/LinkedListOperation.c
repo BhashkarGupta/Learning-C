776288de675623725469305be56cc8accbf0de06
Bhashkar Gupta <bhashkar.spi@gmail.com>
2023-06-28 16:09:36 +0530

    LinkedListOperation

---
+++ Linkedlist/LinkedListOperation.c
@@ -0,0 +1,177 @@
+// Linked List Operations
+#include<stdio.h>
+#include<stdlib.h>
+
+// Structure for Node
+struct Node
+{
+    int data;
+    struct Node *link;
+};
+
+// Creating a fresh Node
+struct Node* createNewNode(int data, struct Node* pointer){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->link = pointer;
+    return newNode;
+}
+
+// Calculate the LinkList size
+int linkedListSize(struct Node* head){
+    if(head == NULL){
+        return 0;
+    }
+    else{
+        int size = 0;
+        struct Node* temp = head;
+        while (temp != NULL)
+        {
+            size++;
+            temp = temp->link;
+        }
+       return size; 
+    }
+}
+
+// Insert a Node
+void insertLinkedList(struct Node** head, int data, int position){
+    if(position < 1){
+        printf("Invalid Position, Insertion failed.\n");
+    }
+    else if (position == 1){
+        struct Node* newNode = createNewNode(data, *head);
+        *head = newNode;
+    }
+    else{
+        int size = linkedListSize(*head);
+        struct Node* tempNode = *head;
+
+        if(position > size){
+            position = size+1;
+        }
+        
+        while ((position-1) > 1){ // -1 beacause the temp node is pointing at node one not head pointer
+            tempNode = tempNode->link;
+            position--;
+        }
+
+        struct Node* newNode = createNewNode(data, tempNode->link);
+        tempNode->link = newNode;
+    }
+}
+
+// Update a Node
+void updateNode(struct Node** head, int data, int position){
+    int size = linkedListSize(*head);
+    if(position<1 || position>size){
+        printf("Invalid Position, Updation failed.\n");
+    }
+    else{
+        struct Node* tempNode = *head;
+        while (position>1){
+            position--;
+            tempNode = tempNode->link;
+        }
+        tempNode->data = data;
+    }
+}
+
+// Delete a node
+void deleteNode(struct Node** head, int position){
+    int size = linkedListSize(*head);
+    if(position<1 || position>size){
+        printf("Invalid Position, deletion failed.\n");
+    }
+    else if (position==1){
+        struct Node* tempNode = *head;
+        *head = tempNode->link;
+    }
+    
+    else{
+        struct Node* previousNode = *head;
+        struct Node* currentNode = previousNode->link;
+        while ((position-1)>1){
+            position--;
+            previousNode = previousNode->link;
+            currentNode = currentNode->link;
+        }
+        previousNode->link = currentNode->link;
+    }
+}
+
+// Print Linked List
+void printLinkedlist(struct Node* head){
+    struct Node* current = head;
+    printf("List: ");
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->link;
+    }
+    printf("\n");
+}
+
+int main(){
+    struct Node* head = NULL; // pointer(Head) for struct Node data type
+    int listSize, tempValue, position, option;
+
+    // Creating a Node
+    printf("Please input the size of list: ");
+    scanf("%d", &listSize);
+    for(int i=0; i<listSize; i++){
+        printf("Please enter the No:");
+        scanf("%d", &tempValue);
+        insertLinkedList(&head, tempValue, 1); // Inserting at beginning
+    }
+    printLinkedlist(head);
+    printf("size: %d\n", linkedListSize(head));
+
+    // Operation menu
+    char cont = 'y';
+    while(cont == 'y'){
+        printf("\nPlease select the function you want to perform:\n");
+        printf("1. Update\n");
+        printf("2. Delete\n");
+        printf("3. Insert\n");
+        printf("Please select(1/ 2/ 3): ");
+        scanf("%d", &option);
+
+        switch (option){
+        case 1:
+            printf("Please enter the data:");
+            scanf("%d", &tempValue);
+            printf("Please input the position:");
+            scanf("%d", &position);
+            updateNode(&head,tempValue, position);
+            printLinkedlist(head);
+            printf("Do you want to continue Operation(y/n): ");
+            scanf(" %c", &cont);
+            break;
+        case 2:
+            printf("Please input the position(to delete):");
+            scanf("%d", &position);
+            deleteNode(&head, position);
+            printLinkedlist(head);
+            printf("Do you want to continue Operation(y/n): ");
+            scanf(" %c", &cont);
+            break;
+        case 3:
+            printf("Please enter the data:");
+            scanf("%d", &tempValue);
+            printf("Please input the position:");
+            scanf("%d", &position);
+            insertLinkedList(&head,tempValue, position);
+            printLinkedlist(head);
+            printf("Do you want to continue Operation(y/n): ");
+            scanf(" %c", &cont);
+            break;
+        default:
+            printf("Invalid Input\n");
+            printf("Do you want to try again(y/n): ");
+            scanf(" %c", &cont);
+            break;
+        }
+    }
+    return 0;
+}
--- Max_Min.c
+++ Max_Min.c
@@ -12,19 +12,19 @@ int main(){
 }
 
 void minMax(int *arr, int *min, int *max){
-    *max = *min = arr[0];
-    int *ptr = arr;
+    *max = *min = arr[0]; // setting value of min and max to the value of 0th index initially
+    int *ptr = arr; // copying the starting address of array to the pointer
     int size = 0;
 
     while (*(ptr + size) != '\0') {
-        size++;
+        size++; // finding out the size of array first
     }
 
     for(int i = 1; i < size; i++){
-        if(*max < *(ptr+i)){
+        if(*max < *(ptr+i)){ // if value at max is less that the current index value, replace it
             *max = *(ptr+i);
         }
-        if(*min > *(ptr+i)){
+        if(*min > *(ptr+i)){ // if value at min is greater than the current index value, replace it.
             *min = *(ptr+i);
         }
     }
